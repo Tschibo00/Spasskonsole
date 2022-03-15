4059283a29fefd470238f4eb9d7d7fca2af29917
Tschibo00 <ballhause@gmx.de>
2022-03-15 11:28:46 +0100

    removed unnecessary code

--- IQGame.h
+++ IQGame.h
@@ -8,24 +8,6 @@ class IQGame: public Game {
 
 		void initLevel();
 		void move();
-
-    bool setChar(uint8_t i,uint8_t n, bool showZero){
-      if (i>4)return false;
-      if ((!showZero)&&(n==0))
-        score[i]=32;
-      else
-        score[i]=48+n;
-      return n!=0;
-    }
-    void setNumber(uint16_t n){
-      bool zero=false;
-      zero|=setChar(0,(n/10000)%10,zero);
-      zero|=setChar(1,(n/1000)%10,zero);
-      zero|=setChar(2,(n/100)%10,zero);
-      zero|=setChar(3,(n/10)%10,zero);
-      zero|=setChar(4,n%10,true);
-      score[5]=32;
-    }
     
 	public:
 		IQGame();
--- MemTestGame.cpp
+++ MemTestGame.cpp
@@ -5,16 +5,6 @@ MemTestGame::MemTestGame(){
 }
 
 void MemTestGame::play(){
-  if (getKeyClick(KEY_1)){
-    if (showingScore<0){
-      if (survived<10) showingScore=64;
-      if (survived>=10) showingScore=48;
-      if (survived>=100) showingScore=32;
-      if (survived>=1000) showingScore=16;
-      if (survived>=10000) showingScore=0;
-    }else
-      showingScore=-1;
-  }
 	clear(CRGB::Black);
 }
 
@@ -25,20 +15,12 @@ void MemTestGame::move(){
  
 	switch (getNumberClick()) {
     case KEY_UP:
-      y--;
-      if (y<-1) y=7;
       break;
     case KEY_DOWN:
-      y++;
-      if (y>7) y=-1;
       break;
     case KEY_LEFT:
-      x--;
-      if (x<0) x=7;
       break;
     case KEY_RIGHT:
-      x++;
-      if (x>7) x=0;
       break;
 	}
 }
